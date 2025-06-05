49e82c0789de9a30420d1899e3b16fdb3208329b
Christine MacDonald <christine.macdonald@digirati.com>
2025-06-05 09:13:29 +0100

    different commit to try the AWS code

--- tests/helpers/helpers.ts
+++ tests/helpers/helpers.ts
@@ -73,6 +73,7 @@ export async function uploadFile(
     const command = new ListObjectsV2Command(input);
     const response = await s3Client.send(command);
     const contents = response.Contents;
+    console.log(contents);
     const stringToFind = `${s3Url.key}${fileToFind}/`;
 
     var result = contents.filter(obj => {
