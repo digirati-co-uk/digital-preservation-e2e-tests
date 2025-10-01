f6293c074f9a4838f6ec0f2a6b45ccac46d1b0ff
Christine MacDonald <christine.macdonald@digirati.com>
2025-10-01 21:27:56 +0100

    Search tests complete

--- tests/userFlows/pages/SearchPage.ts
+++ tests/userFlows/pages/SearchPage.ts
@@ -1,11 +1,13 @@
 import { expect, type Locator, type Page } from '@playwright/test';
 import {NavigationPage} from "./NavigationPage";
 import {DepositPage} from "./DepositPage";
+import {ArchivalGroupPage} from "./ArchivalGroupPage";
 
 export class SearchPage {
   readonly page: Page;
   readonly navigationPage: NavigationPage;
   readonly depositPage: DepositPage;
+  readonly archivalGroupPage: ArchivalGroupPage;
 
   //Search Locators
   readonly searchBox: Locator;
@@ -30,11 +32,26 @@ export class SearchPage {
   readonly faqTabSubHeading2: Locator;
   readonly faqTabSubHeading3: Locator;
   readonly faqTabSubHeading4: Locator;
+  readonly fedoraTabHeading: Locator;
+  readonly fedoraIdColumn: Locator;
+  readonly totalSearchResults: Locator;
+  readonly testImageResult: Locator;
+  readonly testWordDocResult: Locator;
+  readonly metsFileResult: Locator;
+  readonly testImageResultLink: Locator;
+  readonly paginationPage1Button: Locator;
+  readonly paginationPage3Button: Locator;
+  readonly paginationPage5Button: Locator;
+  readonly activePageNumber: Locator;
+  readonly depositTableDateCreatedRows: Locator;
+  readonly fedoraTableDateCreatedRows: Locator;
+  readonly firstDepositLink: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.navigationPage = new NavigationPage(page);
     this.depositPage = new DepositPage(page);
+    this.archivalGroupPage = new ArchivalGroupPage(page);
 
     //Locators
     this.searchBox = page.getByRole('textbox', { name: 'Search' });
@@ -55,6 +72,21 @@ export class SearchPage {
     this.faqTabSubHeading2 = page.getByRole('heading', {name: 'Fedora Search'});
     this.faqTabSubHeading3 = page.getByRole('heading', {name: 'Deposit Search'});
     this.faqTabSubHeading4 = page.getByRole('heading', {name: 'Identifier Search'});
+    this.fedoraTabHeading = page.getByRole('heading', {name: 'Fedora'});
+    this.fedoraIdColumn = page.getByLabel('td-fedoraid');
+    this.totalSearchResults = page.getByLabel('Total items').filter({visible: true});
+    this.testImageResult = this.fedoraIdColumn.getByText(this.depositPage.testImageLocation).first();
+    this.testWordDocResult = this.fedoraIdColumn.getByText(this.depositPage.testWordDocLocation).first();
+    this.metsFileResult = this.fedoraIdColumn.getByText('mets.xml').first();
+    this.testImageResultLink = page.getByRole('row').filter({has: this.testImageResult}).first().getByLabel('td-fedora-link').getByRole('link');
+    this.paginationPage1Button = page.getByLabel('Paging').getByRole('listitem').filter({has: page.getByText('1', {exact:true})});
+    this.paginationPage3Button = page.getByLabel('Paging').getByRole('listitem').filter({has: page.getByText('3', {exact:true})});
+    this.paginationPage5Button = page.getByLabel('Paging').getByRole('listitem').filter({has: page.getByText('5', {exact:true})});
+    this.activePageNumber = page.getByRole('navigation', { name: 'Paging' }).getByLabel('Page number');
+    this.depositTableDateCreatedRows = page.getByRole('table', {name: 'table-deposit-files'}).getByRole('cell', {name: 'td-deposits-created', exact: true});;
+    //TODO raise with Frank that this is named incorrectly
+    this.fedoraTableDateCreatedRows = page.getByRole('table', {name: 'table-deposit-files'}).getByRole('cell', {name: 'td-path', exact: true});
+    this.firstDepositLink = page.getByLabel('td-deposits-link').first().getByRole('link');
   }
 
   async goto() {
--- tests/userFlows/search.spec.ts
+++ tests/userFlows/search.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '../../fixture';
 import {SearchPage} from "./pages/SearchPage";
-import {expect, Page} from "@playwright/test";
+import {BrowserContext, expect, Locator, Page} from "@playwright/test";
 import {generateUniqueId} from "../helpers/helpers";
 
 test.describe('Search Tests', () => {
@@ -11,11 +11,10 @@ test.describe('Search Tests', () => {
     searchPage = new SearchPage(page);
   });
 
-  test(`test the identity service search`, async ({page}) => {
+  test(`Test the identity service search`, async ({page}) => {
 
     await test.step('Search by PID', async() => {
       await executeSearch(searchPage.pidToSearchFor);
-      await checkAllTabsArePresent();
 
       await expect(searchPage.identifierTabResultCount).toHaveText('1');
       await searchPage.identifierTab.click();
@@ -28,7 +27,6 @@ test.describe('Search Tests', () => {
 
     await test.step('Search by IRN', async() => {
       await executeSearch(searchPage.irnToSearchFor);
-      await checkAllTabsArePresent();
 
       await expect(searchPage.identifierTabResultCount).toHaveText('1');
       await searchPage.identifierTab.click();
@@ -38,7 +36,7 @@ test.describe('Search Tests', () => {
     });
   });
 
-  test(`test the deposit service search`, async ({page}) => {
+  test(`Test the deposit service search`, async ({page, context}) => {
 
     //Set a 5-minute timeout
     test.setTimeout(300_000);
@@ -114,6 +112,19 @@ test.describe('Search Tests', () => {
       await checkDepositSearchResults(uniqueTitle, page, uniqueTitle, 1, false);
     });
 
+    await test.step('Check that we can open the deposit in a new tab', async() => {
+      //Check we can open it in another tab
+      const pagePromise = context.waitForEvent('page');
+      await searchPage.firstDepositLink.click();
+      const depositsTab = await pagePromise;
+      await depositsTab.waitForLoadState();
+
+      //Verify we are on the deposit page
+      await expect(depositsTab, 'We have been navigated into the new Deposit page').toHaveURL(searchPage.depositPage.depositsURL);
+      await depositsTab.close();
+
+    });
+
     await test.step('Tidy up and delete the Deposit', async() => {
       //Navigate back into the first deposit in order to delete it
       await page.goto(depositURL);
@@ -122,23 +133,135 @@ test.describe('Search Tests', () => {
     });
   });
 
-  test(`TODO test the Fedora service search`, async ({}) => {
-    //
+  test(`Test the Fedora service search`, async ({page, context}) => {
+
+    //Set a 2-minute timeout
+    test.setTimeout(120_000);
+
+    //This term should return a number of results.
+    await executeSearch(searchPage.depositPage.testValidArchivalURI);
+    await searchPage.fedoraTab.click();
+    //Check the page title is correct
+    await expect(searchPage.fedoraTabHeading, 'We can see the correct page heading').toBeVisible();
+
+    //Check the total items is >500
+    expect(Number.parseInt(await searchPage.totalSearchResults.textContent()), 'We see hundreds of search results').toBeGreaterThanOrEqual(500);
+
+    //Check that some of the fedora id results are for the standard files we put into each archival group
+    //when running the archival group tests
+    await expect(searchPage.testImageResult, 'We can see the test image file in the results').toBeVisible();
+    await expect(searchPage.testWordDocResult, 'We can see the test word doc in the results').toBeVisible();
+    await expect(searchPage.metsFileResult, 'We can see mets files in the results').toBeVisible();
+
+    //Check fedord id in format info:fedora and contains the full path
+    //'_for_testing/playwright-testing/playwright-valid-slug-abcd'
+    for (const fedoraId of (await searchPage.fedoraIdColumn.all())) {
+      await expect(fedoraId, 'The full path to the Fedora object is displayed').toContainText(`info:fedora/${searchPage.navigationPage.basePath}/${searchPage.depositPage.testValidArchivalURI}`);
+    }
+
+    //Check we can open it in another tab
+    const pagePromise = context.waitForEvent('page');
+    await searchPage.testImageResultLink.click();
+    const archivalGroupTab = await pagePromise;
+    await archivalGroupTab.waitForLoadState();
+
+    //Verify we are on the archival group page and within the correct file
+    await expect(archivalGroupTab.getByRole('link', { name: 'Go to Archival Group' })).toBeVisible();
+    await expect(archivalGroupTab.getByRole('heading', {name: searchPage.depositPage.testImageLocation})).toBeVisible();
+    await archivalGroupTab.close();
 
   });
 
-  test(`TODO execute a test directly using query params`, async ({}) => {
+  test(`Execute a search directly using query params`, async ({page}) => {
+    await page.goto(`/Search?text=${searchPage.depositPage.testValidArchivalURI}`);
+    await checkAllTabsArePresent();
+
+    //Check the Fedora results page, should have hundreds of results
+    await searchPage.fedoraTab.click();
+    await expect(searchPage.fedoraTabHeading, 'We can see the correct page heading').toBeVisible();
+    //Check the total items is >500
+    expect(Number.parseInt(await searchPage.totalSearchResults.textContent()), 'We see hundreds of search results').toBeGreaterThanOrEqual(500);
+
+    //Check the deposits result tab, should have hundreds of results
+    await searchPage.depositTab.click();
+    await expect(searchPage.depositTabHeading, 'We can see the correct page heading').toBeVisible();
+    //Check the total items is >500
+    expect(Number.parseInt(await searchPage.totalSearchResults.textContent()), 'We see hundreds of search results').toBeGreaterThanOrEqual(500);
+
+    //Check the identifer tab, should have 0 results
+    await searchPage.identifierTab.click();
+    await expect(searchPage.identifierTabHeading, 'We can see the correct page heading').toBeVisible();
+    //Check the total items is 0
+    expect(Number.parseInt(await searchPage.identifierTabResultCount.textContent()), 'We see no search results').toEqual(0);
 
   });
 
-  test(`TODO check pagination and ordering of large result sets`, async ({}) => {
-    //test step - check the pagination - use search term 'playwright' to get thousands of results
+  test(`Check pagination and ordering of large result sets`, async ({page}) => {
+
+    await test.step('check the pagination', async() => {
+
+      await page.goto(`/Search?text=${searchPage.depositPage.testValidArchivalURI}`);
+      await checkAllTabsArePresent();
+
+      //Check the Fedora results page, should have hundreds of results
+      await searchPage.fedoraTab.click();
+      await expect(searchPage.fedoraTabHeading, 'We can see the correct page heading').toBeVisible();
+
+      //Assert we are currently on page 1 of the results
+      await expect(searchPage.paginationPage1Button, 'Page 1 is set to active in the pagination').toHaveClass('page-item active');
+      await expect(searchPage.activePageNumber, 'Pagination states Page 1 of ...').toHaveText('1');
+      await expect(searchPage.paginationPage3Button, 'Page 3 is set to NOT active in the pagination').not.toHaveClass('page-item active');
+
+      //Click on page 3 of the results and verify it's the one now selected
+      await searchPage.paginationPage3Button.click();
+      await expect(searchPage.paginationPage1Button, 'Page 1 is set to NOT active in the pagination').not.toHaveClass('page-item active');
+      await expect(searchPage.paginationPage3Button, 'Page 3 is set to active in the pagination').toHaveClass('page-item active');
+      await expect(searchPage.activePageNumber, 'Pagination states Page 3 of ...').toHaveText('3');
+
+      //Navigate to the Deposits tab, and change to page 5 of the results
+      await searchPage.depositTab.click();
+      await expect(searchPage.depositTabHeading, 'We can see the correct page heading').toBeVisible();
+
+      //Assert we are currently on page 1 of the results
+      await expect(searchPage.paginationPage1Button, 'Page 1 is set to active in the pagination').toHaveClass('page-item active');
+      await expect(searchPage.activePageNumber, 'Pagination states Page 1 of ...').toHaveText('1');
+      await expect(searchPage.paginationPage5Button, 'Page 3 is set to NOT active in the pagination').not.toHaveClass('page-item active');
+
+      //Click on page 5 of the results and verify it's the one now selected
+      await searchPage.paginationPage5Button.click();
+      await expect(searchPage.paginationPage1Button, 'Page 1 is set to NOT active in the pagination').not.toHaveClass('page-item active');
+      await expect(searchPage.paginationPage5Button, 'Page 5 is set to active in the pagination').toHaveClass('page-item active');
+      await expect(searchPage.activePageNumber, 'Pagination states Page 5 of ...').toHaveText('5');
+
+      //Switch back to the Fedora tab and check that we are still on page 3 there
+      await searchPage.fedoraTab.click();
+      await expect(searchPage.fedoraTabHeading, 'We can see the correct page heading').toBeVisible();
+      await expect(searchPage.paginationPage1Button, 'Page 1 is set to NOT active in the pagination').not.toHaveClass('page-item active');
+      await expect(searchPage.paginationPage3Button, 'Page 3 is set to active in the pagination').toHaveClass('page-item active');
+      await expect(searchPage.activePageNumber, 'Pagination states Page 3 of ...').toHaveText('3');
+    });
+
+    await test.step('Check the ordering', async() => {
+
+      await executeSearch(searchPage.depositPage.testValidArchivalURI);
+
+      //Check the Fedora results page for correct ordering
+      await searchPage.fedoraTab.click();
+      await expect(searchPage.fedoraTabHeading, 'We can see the correct page heading').toBeVisible();
+      searchPage.depositPage.validateSortOrder<Date>((await searchPage.fedoraTableDateCreatedRows.allTextContents()), false, (value) => new Date(value), (a: Date, b:Date) => a.getTime() - b.getTime());
+
+      //Navigate to the Deposits tab, and check for correct ordering
+      await searchPage.depositTab.click();
+      await expect(searchPage.depositTabHeading, 'We can see the correct page heading').toBeVisible();
+      searchPage.depositPage.validateSortOrder<Date>((await searchPage.depositTableDateCreatedRows.allTextContents()), false, (value) => new Date(value), (a: Date, b:Date) => a.getTime() - b.getTime());
+
+    });
+
   });
 
   test(`Check the FAQ tab`, async ({}) => {
     //use a weird search term to get the page to jump straight to the FAQ tab
     await executeSearch('xxxxyyyyxxxxzzzzyyyyyzzzzuuuuiiiiqqqquuuuooooqqqqllllqqqqdddd');
-    await checkAllTabsArePresent();
     //Check we are on the FAQ tab
     await expect(searchPage.faqTabHeading, 'The tab page heading is visible').toBeVisible();
     //Check the various sub headings are visible
@@ -149,7 +272,7 @@ test.describe('Search Tests', () => {
 
   });
 
-  test(`FAILING TILL BUG FIXED Check that no search term directs to the FAQ page`, async ({}) => {
+  test(`Check that no search term directs to the FAQ page`, async ({}) => {
     //use an empty search term
     await executeSearch(' ');
     //Check we are on the FAQ tab
@@ -165,7 +288,7 @@ test.describe('Search Tests', () => {
   async function checkDepositSearchResults(searchTerm: string, page: Page, depositName: string, expectedResults: number, exact: boolean = true){
     //Check we get one result using the archival group location
     await executeSearch(searchTerm);
-    await checkAllTabsArePresent();
+
     if (exact) {
       //Check the correct number of results displayed on the results page
       await expect(searchPage.depositTabResultCount).toHaveText(expectedResults.toString());
@@ -185,6 +308,7 @@ test.describe('Search Tests', () => {
     await searchPage.getStarted();
     await searchPage.searchBox.fill(searchTerm);
     await searchPage.searchBox.press('Enter');
+    await checkAllTabsArePresent();
   }
 
   async function checkAllTabsArePresent(){
@@ -192,7 +316,7 @@ test.describe('Search Tests', () => {
     await expect(searchPage.depositTab, 'The Deposits results tab is visible').toBeVisible();
     await expect(searchPage.identifierTab, 'The Identifier results tab is visible').toBeVisible();
     await expect(searchPage.faqTab, 'The FAQ results tab is visible').toBeVisible();
-    await expect(searchPage.allTabs).toHaveCount(4);
+    await expect(searchPage.allTabs, 'There are only 4 tabs').toHaveCount(4);
   }
 
   async function checkDepositTabResults(page: Page, depositName: string, expectedResults: number, exact:boolean = true){
